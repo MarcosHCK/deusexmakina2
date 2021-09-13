5a33d67dfd69720119ad800acaec0ce09c89905e
MarcosHCK <marcoshck16@gmail.com>
2021-09-12 20:24:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# deusexmakina2
+
+---
+
+### Homepage
+
+I am still working on that
+
+### Changelog
+
+See [NEWS](https://github.com/MarcosHCK/deusexmakina2/blob/master/NEWS) for details on changes and fixes made in the current release.
---
+++ src/ds_application.h
@@ -0,0 +1,30 @@
+/*  Copyright 2021-2022 MarcosHCK
+ *  This file is part of deusexmakina2.
+ *
+ *  deusexmakina2 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  deusexmakina2 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with deusexmakina2.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __DS_APPLICATION_INCLUDED__
+#define __DS_APPLICATION_INCLUDED__ 1
+#include <gio.h>
+
+#if __cpluscplus
+extern "C" {
+#endif // __cpluscplus
+
+#if __cpluscplus
+}
+#endif // __cpluscplus
+
+#endif // __DS_APPLICATION_INCLUDED__
